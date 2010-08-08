c4122d48070dd61793b81a6f3da4ab13328579e7
young.jiandong <young.jiandong@gmail.com>
2010-08-08 22:37:30 +0800

    jetty add ssl...

--- application/sshapp/bin/jetty-debug.bat
+++ application/sshapp/bin/jetty-debug.bat
@@ -4,6 +4,6 @@ echo [INFO] Use maven jetty-plugin run the project, with debug options.
 cd %~dp0
 cd ..
 set MAVEN_OPTS=-Xmx512m -XX:MaxPermSize=256m -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=8000
-call mvn jetty:run -Dmaven.test.skip=true
+call mvn -o jetty:run -Dmaven.test.skip=true
 cd bin
 pause
