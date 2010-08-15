5689d955915965ad0ef3dd4b8d61b6f198502d12
young.jiandong <young.jiandong@gmail.com>
2010-08-15 16:05:34 +0800

    update..

--- examples/showcase/src/main/java/org/springside/examples/showcase/cache/MemcachedObjectType.java
+++ examples/showcase/src/main/java/org/springside/examples/showcase/cache/MemcachedObjectType.java
@@ -4,30 +4,32 @@ import org.springside.examples.showcase.common.service.AccountManager;
 
 /**
  * 统一定义Memcached中存储的各种对象的Key前缀和超时时间.
- * 
+ *
  * Memcached使用在AccountManager中.
- * 
+ *
  * @see AccountManager#getLoadedUser(String)
- * 
+ *
  * @author calvin
  */
 public enum MemcachedObjectType {
-	USER("user:", 60 * 60 * 1);
+    //other example
+    POST("post:", 60 * 60),
+    USER("user:", 60 * 60 * 1);
 
-	private String prefix;
-	private int expiredTime;
+    private String prefix;
+    private int expiredTime;
 
-	MemcachedObjectType(String prefix, int expiredTime) {
-		this.prefix = prefix;
-		this.expiredTime = expiredTime;
-	}
+    MemcachedObjectType(String prefix, int expiredTime) {
+        this.prefix = prefix;
+        this.expiredTime = expiredTime;
+    }
 
-	public String getPrefix() {
-		return prefix;
-	}
+    public String getPrefix() {
+        return prefix;
+    }
 
-	public int getExpiredTime() {
-		return expiredTime;
-	}
+    public int getExpiredTime() {
+        return expiredTime;
+    }
 
 }
