c67af6c20a8ce4ecaa6c0bca65bfdba17e93a928
yangjiandong <young.jiandong@gmail.com>
2010-09-26 17:24:50 +0800

    sshapp test error..

--- application/sshapp/src/test/java/org/ssh/app/functional/BaseFunctionalTestCase.java
+++ application/sshapp/src/test/java/org/ssh/app/functional/BaseFunctionalTestCase.java
@@ -5,15 +5,16 @@ import java.util.Properties;
 import javax.sql.DataSource;
 
 import org.junit.AfterClass;
-import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
+import org.junit.runner.RunWith;
 import org.mortbay.jetty.Server;
 import org.openqa.selenium.WebDriver;
+import org.springside.modules.test.groups.GroupsTestRunner;
 import org.springside.modules.test.utils.DbUnitUtils;
 import org.springside.modules.test.utils.JettyUtils;
 import org.springside.modules.test.utils.SeleniumUtils;
-import org.springside.modules.utils.PropertyUtils;
+import org.springside.modules.utils.PropertiesUtils;
 import org.springside.modules.utils.SpringContextHolder;
 import org.ssh.app.Start;
 
@@ -25,61 +26,72 @@ import org.ssh.app.Start;
  * @author calvin
  */
 @Ignore
-public class BaseFunctionalTestCase extends Assert {
+@RunWith(GroupsTestRunner.class)
+public class BaseFunctionalTestCase {
 
     protected static final String BASE_URL = Start.BASE_URL;
 
-    protected static Server server;
+    protected static Server jettyServer;
 
-    protected static DataSource dataSource;
+    protected static DataSource dataSourceHolder;
 
     protected static WebDriver driver;
 
     @BeforeClass
-    public static void init() throws Exception {
-        if (server == null) {
-            startJetty();
-            fetchDataSource();
-        }
+    public static void startAll() throws Exception {
+        startJetty();
 
+        fetchDataSource();
         loadDefaultData();
     }
 
-    /**
-     * 删除默认数据.
-     */
     @AfterClass
-    public static void cleanDefaultData() throws Exception {
-        DbUnitUtils.removeData(dataSource, "/data/default-data.xml");
+    public static void stopAll() throws Exception {
+        cleanDefaultData();
+        stopJetty();
     }
 
     /**
-     * 启动Jetty服务器
+     * 启动Jetty服务器.
      */
     protected static void startJetty() throws Exception {
-        server = JettyUtils.buildTestServer(Start.PORT, Start.CONTEXT);
-        server.start();
+        jettyServer = JettyUtils.buildTestServer(Start.PORT, Start.CONTEXT);
+        jettyServer.start();
+    }
+
+    /**
+     * 关闭Jetty服务器.
+     */
+    protected static void stopJetty() throws Exception {
+        jettyServer.stop();
     }
 
     /**
      * 取出Jetty Server内的DataSource.
      */
     protected static void fetchDataSource() {
-        dataSource = SpringContextHolder.getBean("dataSource");
+        dataSourceHolder = SpringContextHolder.getBean("dataSource");
     }
 
     /**
      * 载入默认数据.
      */
     protected static void loadDefaultData() throws Exception {
-        DbUnitUtils.loadData(dataSource, "/data/default-data.xml");
+        DbUnitUtils.loadData(dataSourceHolder, "/data/default-data.xml");
+    }
+
+    /**
+     * 删除默认数据.
+     */
+    public static void cleanDefaultData() throws Exception {
+        DbUnitUtils.removeData(dataSourceHolder, "/data/default-data.xml");
     }
 
     /**
      * 创建WebDriver.
      */
     protected static void createWebDriver() throws Exception {
-        Properties props = PropertyUtils.loadProperties("classpath:/application.test.properties",
+        Properties props = PropertiesUtils.loadProperties("classpath:/application.test.properties",
                 "classpath:/application.test-local.properties");
 
         driver = SeleniumUtils.buildDriver(props.getProperty("selenium.driver"));
